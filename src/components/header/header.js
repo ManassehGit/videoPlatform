78a83ce0b266f627fa162bc048f9b0bdd296961a
Manasseh <manasseh.sarfo@amalitech.org>
2022-04-13 16:21:48 +0100

    add header component

---
+++ src/components/header/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const header = () => {
+  return (
+    <div>
+        <img src='VideoPlatformProject.svg' alt='Video Platform Logo' />
+        <TopButtons />
+    </div>
+  )
+}
+
+export default header
