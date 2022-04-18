99e0ad950b7bc58cd3fb35c09f0acf4c48b1e169
Michael Hagan <michael.hagan@amalitech.org>
2022-04-18 18:03:50 +0000

    pages

--- platform/src/App.js
+++ platform/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './components/header/header';
 import Footer from './components/footer/footer';
 import Body from './components/body/body';
+import Form from './components/Form/form';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       button1="Log In"
       button2= "Sign Up"/>
       <Body />
+      <Form />
       <Footer />
     </div>
   );
@@ -19,3 +21,7 @@ export default App;
 
 // vziakwwy - public
 // 63878da4-6eb0-42a6-b8eb-f9b3833bcc81 -
+    // "build": "cd platform && npm install && npm run build",
+    // "server": "nodemon server.js",
+    // "platform": "npm start --prefix platform",
+    // "dev": "concurrently \"npm run server\" \"npm run platform\""
