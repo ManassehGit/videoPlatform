bdaa42075397543a94af25feead8ca8acce37a4e
eamalitech <eric.aazore@amalitech.org>
2022-04-19 14:22:36 +0000

    useNavigate

--- src/components/header/TopButtons.js
+++ src/components/header/TopButtons.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import Button from '../Buttons/Button'
-
+import { useNavigate } from "react-router-dom";
 
 const TopButtons = ({button1,button2,color}) => {
+  let navigate = useNavigate(); 
+  const routeChange = () =>{ 
+    let path = `/Form/form`; 
+    navigate(path);
+  }
 
   return (
     <div className='topButtons'>
-      <Button 
+      <Button onClick={routeChange}
       name={button1}
       size="15px"
       color={color}
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -6,9 +6,6 @@ const Header = ({button1,button2}) => {
   return (
     <div className='header'>
       <img src='VideoPlatform.png' alt='Video Platform Logo' />
-      <div className="navbar-start">
-        <a href="/Form/form" className="navbar-item">SigIN</a>
-      </div>
      <TopButtons
       button1={button1}
       button2={button2}
